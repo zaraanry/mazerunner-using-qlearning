03d98220d7d7c24e36a2e22310300b1e93e65919
Zahra Noury <zaraanry@gmail.com>
2019-11-05 19:20:01 +0330

    FIX BUG

--- models/qrandom.py
+++ models/qrandom.py
@@ -25,6 +25,60 @@ class AbstractModel(ABC):
     @abstractmethod
     def predict(self, state):
         """ Predict value based on state. """
+        passfrom abc import ABC, abstractmethod
+from abc import ABC, abstractmethod
+
+class AbstractModel(ABC):
+    def __init__(self, maze, **kwargs):
+        self.environment = maze
+        self.name = kwargs.get("name", "model")
+
+    def load(self, filename):
+        """ Load model from file. """
+        pass
+
+    def save(self, filename):
+        """ Save model to file. """
+        pass
+
+    def train(self, stop_at_convergence, **kwargs):
+        """ Train model. """
+        pass
+
+    @abstractmethod
+    def q(self, state):
+        """ Return q values for state. """
+        pass
+
+    @abstractmethod
+    def predict(self, state):
+        """ Predict value based on state. """
+        pass
+class AbstractModel(ABC):
+    def __init__(self, maze, **kwargs):
+        self.environment = maze
+        self.name = kwargs.get("name", "model")
+
+    def load(self, filename):
+        """ Load model from file. """
+        pass
+
+    def save(self, filename):
+        """ Save model to file. """
+        pass
+
+    def train(self, stop_at_convergence, **kwargs):
+        """ Train model. """
+        pass
+
+    @abstractmethod
+    def q(self, state):
+        """ Return q values for state. """
+        pass
+
+    @abstractmethod
+    def predict(self, state):
+        """ Predict value based on state. """
         passimport random
 
 import numpy as np
