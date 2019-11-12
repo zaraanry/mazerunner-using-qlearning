ec6ea2623845fa259176f7a0ce3e1bd35d5ecfdd
Zahra Noury <zaraanry@gmail.com>
2019-11-12 23:07:47 +0330

    FIX BUG

--- models/__init__.py
+++ models/__init__.py
@@ -25,7 +25,59 @@ class AbstractModel(ABC):
     @abstractmethod
     def predict(self, state):
         """ Predict value based on state. """
-        passfrom .abstractmodel import *
+        passfrom abc import ABC, abstractmethod
+
+class AbstractModel(ABC):
+    def __init__(self, maze, **kwargs):
+        self.environment = maze
+        self.name = kwargs.get("name", "model")
+
+    def load(self, filename):
+        """ Load model from file. """
+        pass
+
+    def save(self, filename):
+        """ Save model to file. """
+        pass
+
+    def train(self, stop_at_convergence, **kwargs):
+        """ Train model. """
+        pass
+
+    @abstractmethod
+    def q(self, state):
+        """ Return q values for state. """
+        pass
+
+    @abstractmethod
+    def predict(self, state):
+        """ Predict value based on state. """
+        passimport random
+
+import numpy as np
+
+from models import AbstractModel
+
+
+class RandomModel(AbstractModel):
+    """ Prediction model which randomly chooses the next action. """
+
+    def __init__(self, game):
+        super().__init__(game)
+
+    def q(self, state):
+        """ Return Q value for all action for a certain state.
+
+            :return np.ndarray: Q values
+        """
+        return np.array([0, 0, 0, 0])
+
+    def predict(self, **kwargs):
+        """ Randomly choose the next action.
+
+            :return int: Chosen action.
+        """
+        return random.choice(self.environment.actions)from .abstractmodel import *
 from .qrandom import *
 from .qreplaynetwork import *
 from .qtable import *
