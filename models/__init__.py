c90c7a4b1b17cceb720ff0f173198c18a55d6b6c
Zahra Noury <Zaraanry@gmail.com>
2020-05-17 18:34:56 +0430

    MazeRunner Using QLearning

--- models/__init__.py
+++ models/__init__.py
@@ -29,6 +29,36 @@ class RandomModel(AbstractModel):
 
             :return int: Chosen action.
         """
+        return random.choice(self.environment.actions)
+import random
+
+import numpy as np
+
+from models import AbstractModel
+
+
+class RandomModel(AbstractModel):
+    """ Prediction model which randomly chooses the next action. """
+
+    def __init__(self, game):
+        super().__init__(game)
+
+    def q(self, state):
+        """ Return Q value for all action for a certain state.
+
+            :return np.ndarray: Q values
+
+
+
+
+        """
+        return np.array([0, 0, 0, 0])
+
+    def predict(self, **kwargs):
+        """ Randomly choose the next action.
+
+            :return int: Chosen action.
+        """
         return random.choice(self.environment.actions)from .abstractmodel import *
 from .qrandom import *
 from .qreplaynetwork import *
