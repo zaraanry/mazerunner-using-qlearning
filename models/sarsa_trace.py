06563c11e4ed5fad73e2bcea94328a8400995657
Zahra Noury <Zaraanry@gmail.com>
2020-06-01 10:11:20 +0430

    MazeRunner Using QLearning

--- models/sarsa_trace.py
+++ models/sarsa_trace.py
@@ -106,6 +106,33 @@ class AbstractModel(ABC):
     @abstractmethod
     def predict(self, state):
         """ Predict value based on state. """
+        passfrom abc import ABC, abstractmethod
+
+class AbstractModel(ABC):
+    def __init__(self, maze, **kwargs):
+        self.environment = maze
+        self.name = kwargs.get("name", "model")
+
+    def load(self, filename):
+        """ Load model from file. """
+        pass
+
+    def save(self, filename):
+        """ Save model to file. """
+        pass
+
+    def train(self, stop_at_convergence, **kwargs):
+        """ Train model. """
+        pass
+
+    @abstractmethod
+    def q(self, state):
+        """ Return q values for state. """
+        pass
+
+    @abstractmethod
+    def predict(self, state):
+        """ Predict value based on state. """
         passimport logging
 import random
 from datetime import datetime
