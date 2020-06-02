7c12f0702d4987d3207ae633b6ead97461232bb3
Zahra Noury <Zaraanry@gmail.com>
2020-06-02 12:38:01 +0430

    MazeRunner Using QLearning

--- models/abstractmodel.py
+++ models/abstractmodel.py
@@ -18,6 +18,11 @@ from .qreplaynetwork import *
 from .qtable import *
 from .qtable_trace import *
 from .sarsa import *
+from .sarsa_trace import *from .qrandom import *
+from .qreplaynetwork import *
+from .qtable import *
+from .qtable_trace import *
+from .sarsa import *
 from .sarsa_trace import *from abc import ABC, abstractmethod
 
 class AbstractModel(ABC):
