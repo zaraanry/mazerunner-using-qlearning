11d29f49f886f8d0b22173d66e20109910d48784
Zahra Noury <Zaraanry@gmail.com>
2020-06-02 12:37:41 +0430

    MazeRunner Using QLearning

--- environment/__init__.py
+++ environment/__init__.py
@@ -13,4 +13,9 @@ from .qreplaynetwork import *
 from .qtable import *
 from .qtable_trace import *
 from .sarsa import *
+from .sarsa_trace import *from .qrandom import *
+from .qreplaynetwork import *
+from .qtable import *
+from .qtable_trace import *
+from .sarsa import *
 from .sarsa_trace import *
