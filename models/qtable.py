bc2bea6fc1fef61c20eb8defb089372c041c6cc8
Zahra Noury <zaraanry@gmail.com>
2020-06-23 08:47:03 +0430

    MazeRunner Using QLearning

--- models/qtable.py
+++ models/qtable.py
@@ -26,6 +26,10 @@ import face_recognitionimport numpy as np
 import dlib
 from imutils import face_utils
 from keras.models import load_model
+import face_recognitionimport numpy as np
+import dlib
+from imutils import face_utils
+from keras.models import load_model
 import face_recognitionimport random
 from datetime import datetime
 
