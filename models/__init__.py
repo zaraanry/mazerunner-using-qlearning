c25b1ebfa0bbcf0a39e6c881dca82cd1a2dce1f7
Zahra Noury <zaraanry@gmail.com>
2020-07-10 09:24:25 +0430

    MazeRunner Using QLearning

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,6 @@
-import numpy as np
-import dlib
-from imutils import face_utils
-from keras.models import load_model
-import face_recognition
+from .qrandom import *
+from .qreplaynetwork import *
+from .qtable import *
+from .qtable_trace import *
+from .sarsa import *
+from .sarsa_trace import *
